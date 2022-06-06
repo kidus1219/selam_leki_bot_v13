0e648bcd87757add1c4424fbd81101a31eb78bae
Kidus Haile <kidus1219mail@gmail.com>
2022-06-06 10:34:52 -0700

    cmt

--- const.py
+++ const.py
@@ -1,8 +1,8 @@
 TEXT_PH = "]_["
 ONE_PAGE_LENGTH = 10
 STAR_ICON = ""
-#MAIN_HOST = "https://selam-leki.herokuapp.com/"
-MAIN_HOST = "https://50c6-197-156-107-35.in.ngrok.io/"
+MAIN_HOST = "https://selam-leki.herokuapp.com/"
+#MAIN_HOST = "https://50c6-197-156-107-35.in.ngrok.io/"
 
 
 """
