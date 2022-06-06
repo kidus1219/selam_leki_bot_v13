f222ca85a14ca4b05f508826f3246b1539b6469f
Kidus Haile <kidus1219mail@gmail.com>
2022-06-06 11:37:28 -0700

    cmt

--- main.py
+++ main.py
@@ -28,15 +28,26 @@ def get_bot_pass(update: Update, context: CallbackContext):
     update.message.reply_text(str(context.bot_data['bot_pass']))
 
 
+def set_tli(update, context):
+    if len(context.args) > 0:
+        try:
+            x = int(context.args[0])
+        except Exception as e:
+            print(e)
+        else:
+            application.bot_data['today_leaderboard_initial'] = int(context.args[0])
+
+
 if __name__ == '__main__':
     # updater = Updater(TOKEN, persistence=PostgresPersistence(url=DATABASE_URL))
     updater = Updater(TOKEN)
     application = updater.dispatcher
     application.bot_data['bot_pass'] = {'convo_persist_pass': "1234", 'user_data_pass': "5678"}
-    application.bot_data['today_leaderboard_initial'] = 21
+    application.bot_data['today_leaderboard_initial'] = 1
     View.application = application
 
     application.add_handler(TypeHandler(Update, cleaner), -1)
+    application.add_handler(CommandHandler('stli', set_tli, filters=Filters.user(user_id=[543495028])))
     application.add_handler(primary_convo)
     application.add_handler(CommandHandler('get_bot_pass', get_bot_pass, filters=Filters.user(user_id=[543495028])))
 
