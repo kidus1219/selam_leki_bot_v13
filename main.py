45fde46e34f1445bdb0754418f46769d52e25e8c
Kidus Haile <kidus1219mail@gmail.com>
2022-06-06 11:40:46 -0700

    cmt

--- main.py
+++ main.py
@@ -34,13 +34,15 @@ def set_tli(update, context):
             x = int(context.args[0])
         except Exception as e:
             print(e)
+            update.message.reply_text(str(e))
         else:
             application.bot_data['today_leaderboard_initial'] = int(context.args[0])
+            update.message.reply_text("DONE")
 
 
 if __name__ == '__main__':
-    # updater = Updater(TOKEN, persistence=PostgresPersistence(url=DATABASE_URL))
-    updater = Updater(TOKEN)
+    updater = Updater(TOKEN, persistence=PostgresPersistence(url=DATABASE_URL))
+    # updater = Updater(TOKEN)
     application = updater.dispatcher
     application.bot_data['bot_pass'] = {'convo_persist_pass': "1234", 'user_data_pass': "5678"}
     application.bot_data['today_leaderboard_initial'] = 1
@@ -48,9 +50,10 @@ if __name__ == '__main__':
 
     application.add_handler(TypeHandler(Update, cleaner), -1)
     application.add_handler(CommandHandler('stli', set_tli, filters=Filters.user(user_id=[543495028])))
-    application.add_handler(primary_convo)
     application.add_handler(CommandHandler('get_bot_pass', get_bot_pass, filters=Filters.user(user_id=[543495028])))
+    application.add_handler(primary_convo)
 
-    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://selam-leki-bot-v13.herokuapp.com/' + TOKEN, drop_pending_updates=True)
+    updater.start_webhook("0.0.0.0", PORT, TOKEN, webhook_url='https://selam-leki-bot-v13.herokuapp.com/' + TOKEN,
+                          drop_pending_updates=True)
     # updater.start_polling(drop_pending_updates=True)
     updater.idle()
